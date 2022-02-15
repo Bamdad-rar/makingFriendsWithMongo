f78cd997cdcb71f2b9c15d724148fd374c825d7d
Bamdad <bamdad.rar@protonmail.com>
2022-02-15 20:40:43 +0330

    creating frontend stuff

--- report/views.py
+++ report/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
+from utils import get_db_handle, get_collection_handle
+
 
 
 def report_generator_page(request):
+    if request.method == "POST":
+        # validate input data
+        # conn to db
+        db_handle , db_client = get_db_handle('test','127.0.0.1',27017,"bamdad","1234")
+        coll_handle = get_collection_handle(db_handle,'people')
+        print(request.POST)
+        print(coll_handle.find({}))
     return render(request,'report.html')
 
 
