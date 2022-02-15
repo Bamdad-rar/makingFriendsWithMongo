cc2171745c061cde361954bd75ec4d39cd3c30e3
Bamdad <bamdad.rar@protonmail.com>
2022-02-16 01:20:50 +0330

    added some visual candies, added csv writer

--- api/views.py
+++ api/views.py
@@ -12,15 +12,16 @@ STORAGE_ABS_PATH = BASE_DIR / "report/storage/"
 
 @api_view(['POST'])
 def report(request):
-    START_DATE = "2010-01-01"
-    END_DATE = "2010-12-12"
+    print(request.data)
+    # START_DATE = "2010-01-01"
+    # END_DATE = "2010-12-12"
 
-    db_handle , db_client = get_db_handle()
-    coll_handle = get_collection_handle(db_handle)
+    # db_handle , db_client = get_db_handle()
+    # coll_handle = get_collection_handle(db_handle)
 
-    with open(str(STORAGE_ABS_PATH)+generate_csv_file_name(),'w') as f:
-        writer = csv.writer(f)
-        for row in list(coll_handle.find({"signup":{"$gte":START_DATE,"$lt":END_DATE}},{"_id":0}).sort("signup")):
-            writer.writerow(row.values())
+    # with open(str(STORAGE_ABS_PATH)+generate_csv_file_name(),'w') as f:
+    #     writer = csv.writer(f)
+    #     for row in list(coll_handle.find({"signup":{"$gte":START_DATE,"$lt":END_DATE}},{"_id":0}).sort("signup")):
+    #         writer.writerow(row.values())
 
     return Response(status=status.HTTP_200_OK)
